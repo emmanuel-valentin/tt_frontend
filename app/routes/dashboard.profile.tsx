898f915db2c59d0ea56b188f51b388f54a9a64e9
Emmanuel Valentin <emmanuel-valentin@outlook.com>
2025-03-05 19:16:58 -0600

    Add Badge and Separator components; implement profile detail pages

--- app/routes/dashboard.profile.tsx
+++ app/routes/dashboard.profile.tsx
@@ -1,7 +1,36 @@
+import { Badge } from '~/components/ui/badge';
+import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card';
+import { Separator } from '~/components/ui/separator';
+
+import { UpdateProfileForm } from '~/components/dashboard/profile/update-profile-form';
+import { UserAvatar } from '~/components/shared/avatar/user-avatar';
+
 export default function DashboardMyProfilePage() {
   return (
-    <div>
-      <h1>My Profile Detail</h1>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-2xl">Mi perfil</CardTitle>
+      </CardHeader>
+
+      <CardContent>
+        <div className="flex flex-col md:flex-row items-center md:items-start  gap-4 mb-6">
+          <UserAvatar className="w-16 h-16" />
+          <div className="text-center md:text-start">
+            <p className="text-xl tracking-tight line-clamp-1">
+              {'Emmanuel Guadalupe Valentin Valentin'}
+            </p>
+            <Badge capitalize>{'paciente'}</Badge>
+          </div>
+        </div>
+
+        <h3 className="text-lg font-semibold tracking-tight">
+          Detalles personales
+        </h3>
+
+        <Separator className="mb-4" />
+
+        <UpdateProfileForm />
+      </CardContent>
+    </Card>
   );
 }
--- app/routes/dashboard.profile_.$id.tsx
+++ app/routes/dashboard.profile_.$id.tsx
@@ -1,7 +1,32 @@
+import { UserDetails } from '~/components/dashboard/profile/user-details';
+import { UserAvatar } from '~/components/shared/avatar/user-avatar';
+import { Badge } from '~/components/ui/badge';
+import { Card, CardContent, CardHeader } from '~/components/ui/card';
+import { Separator } from '~/components/ui/separator';
+
 export default function DashboardProfileDetailsPage() {
   return (
-    <div>
-      <h1>Profile Detail</h1>
-    </div>
+    <Card>
+      <CardHeader className="flex flex-col md:flex-row items-center md:items-start gap-4">
+        <div className="flex flex-col md:flex-row items-center md:items-start gap-4">
+          <UserAvatar className="w-16 h-16" />
+          <div className="text-center md:text-start">
+            <p className="text-xl tracking-tight line-clamp-1">
+              {'Emmanuel Guadalupe Valentin Valentin'}
+            </p>
+            <Badge capitalize>{'paciente'}</Badge>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <h3 className="text-lg font-semibold tracking-tight">
+          Detalles personales
+        </h3>
+
+        <Separator className="mb-4" />
+
+        <UserDetails />
+      </CardContent>
+    </Card>
   );
 }
