852bfc1d4c9ec2f56f2d5447e2eeb622cd45aad4
Emmanuel Valentin <emmanuel-valentin@outlook.com>
2025-03-27 10:37:25 -0600

    Fix extra scroll issue in dashboard layout

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -5,11 +5,11 @@ import { Topbar } from "~/components/dashboard/topbar/topbar";
 
 export default function DashboardLayout() {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col min-h-screen">
       <Topbar />
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
-        <main className="mx-auto p-4 w-full bg-muted/60 overflow-y-scroll">
+        <main className="mx-auto p-4 w-full bg-muted/60 overflow-y-auto">
           <Outlet />
         </main>
       </div>
