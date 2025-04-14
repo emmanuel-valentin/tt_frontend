18f0bf4949d7075d3d5d4a23915c9a5d7c794b71
Emmanuel Valentin <emmanuel-valentin@outlook.com>
2025-04-13 19:41:45 -0600

    Fix typo in authentication comment

--- app/api/fisiogoApi.ts
+++ app/api/fisiogoApi.ts
@@ -9,7 +9,7 @@ export const fisiogoApi = axios.create({
 fisiogoApi.interceptors.request.use((config) => {
   // TODO: Read the token from Remix session cookies or the local storage.
   // Later, we'll define how to store the token obtained from the back-end
-  // when a user auhthenticates. But, whatever we store it, we need to
+  // when a user auhthenticate. But, whatever we store it, we need to
   // modify the request and include the Authorization header setting the token as value.
   return config;
 });
