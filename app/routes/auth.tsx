fb90a5af87dbcbf8ac7294ce1d957c17dcad7ebf
Emmanuel Valentin <emmanuel-valentin@outlook.com>
2025-05-15 22:41:07 -0600

    style: Add background color to AuthLayout main section

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -18,7 +18,7 @@ export function clientLoader({ request }: ClientLoaderFunctionArgs) {
 
 export default function AuthLayout() {
   return (
-    <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-secondary">
       <div className="w-full max-w-lg">
         <Outlet />
       </div>
